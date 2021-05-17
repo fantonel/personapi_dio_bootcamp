27781e1a2a4684127698996e3fc5e55e7c3e6a4e
Fábio Antonelli <64748177+fantonel@users.noreply.github.com>
2021-05-17 20:37:44 -0300

    Criação do usuário/telefone (Person e Phone)

---
+++ src/main/java/ead/dio/bootcamp/codeanywhere/personapi/entity/Phone.java
@@ -0,0 +1,46 @@
+package ead.dio.bootcamp.codeanywhere.personapi.entity;
+
+import ead.dio.bootcamp.codeanywhere.personapi.enums.PhoneOperator;
+import ead.dio.bootcamp.codeanywhere.personapi.enums.PhoneType;
+import ead.dio.bootcamp.codeanywhere.personapi.enums.PhoneUsage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "dio", name = "phone")
+public class Phone {
+    @Id
+    @SequenceGenerator(name = "phone_seq", sequenceName = "phone_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_seq")
+    @Column(nullable = false)
+    private Long id;
+    @Column(nullable = false, length = 2)
+    private String ddi;
+    @Column(nullable = false, length = 2)
+    private String ddd;
+    @Column(nullable = false, length = 5)
+    private String prefix;
+    @Column(nullable = false, length = 6)
+    private String suffix;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "phonetype_id", nullable = false)
+    private PhoneType phoneType;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "phoneusage_id", nullable = false)
+    private PhoneUsage phoneUsage;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "phoneoperator_id", nullable = false)
+    private PhoneOperator phoneOperator;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, targetEntity = Person.class)
+    private Person person;
+}
